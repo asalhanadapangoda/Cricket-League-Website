cc476ff65d7412f2b84c3fa8f6b1c898a855ed13
YasiruUpananda <sasmithaupananda2221@gmail.com>
2025-09-13 20:56:03 +0530

    Update adminDashboard.php

--- adminDashboard.php
+++ adminDashboard.php
@@ -1,7 +1,5 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
+session_start();
 
 // Prevent unauthorized access
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
