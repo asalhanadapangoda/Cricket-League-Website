cf59855bfbf0bf451bda22deedb0623f6f5b33c0
Asal himsanda <150653318+asalhanadapangoda@users.noreply.github.com>
2025-09-16 14:36:56 +0530

    Update team page

--- teamColombo.php
+++ teamColombo.php
@@ -4,7 +4,7 @@
     $sql_1 = "SELECT 
             p.first_name,
             p.last_name,
-            pp.type AS role
+            p.role AS role
           FROM player p
           JOIN player_performance pp ON p.player_id = pp.player_id
           WHERE p.team_id = 'T01'";
