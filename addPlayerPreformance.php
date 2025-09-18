26fbb3a94338ade88d9271c9d8d353119637c15d
Asal himsanda <150653318+asalhanadapangoda@users.noreply.github.com>
2025-09-19 00:11:59 +0530

    update

--- addPlayerPreformance.php
+++ addPlayerPreformance.php
@@ -1,5 +1,8 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
     header("Location: adminLogin.php");
     exit;
