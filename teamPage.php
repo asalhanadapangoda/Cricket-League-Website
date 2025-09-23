f76fa5bd71d7b4825a30482cd38a7c5cd17fc5fd
Asal himsanda <150653318+asalhanadapangoda@users.noreply.github.com>
2025-09-23 21:47:54 +0530

    change css

--- teamPage.php
+++ teamPage.php
@@ -30,7 +30,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS_File/teamstyle.css">
+    <link rel="stylesheet" href="CSS_File/teamPage.css">
 </head>
 <body class="team-page">
 <?php if (!$team) { ?>
